0f6d7757a980748224e6e05736dbb565aabd02fb
samberkhout <144678304+samberkhout@users.noreply.github.com>
2025-06-05 13:22:24 +0200

    fix auth import for bcryptjs

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -2,7 +2,7 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 import { NuxtAuthHandler } from '#auth'
 import { prisma } from '../../prisma'
 import { PrismaAdapter } from '@auth/prisma-adapter'
-import { compare } from 'bcryptjs'
+import bcrypt from 'bcryptjs'
 
 export default NuxtAuthHandler({
   secret: process.env.AUTH_SECRET,
@@ -20,7 +20,7 @@ export default NuxtAuthHandler({
         }
         const user = await prisma.user.findUnique({ where: { email: credentials.email } })
         if (!user) return null
-        const valid = await compare(credentials.password, user.password)
+        const valid = await bcrypt.compare(credentials.password, user.password)
         if (!valid) return null
         return { id: user.id, email: user.email }
       }
